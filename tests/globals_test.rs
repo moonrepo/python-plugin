a8094f04afd952d307bc3c006712ceee5400674f
Miles Johnson <mileswjohnson@gmail.com>
2023-09-08 18:23:34 -0700

    Add tests.

---
+++ tests/globals_test.rs
@@ -0,0 +1,5 @@
+// use proto_pdk_test_utils::*;
+
+// // Currently times out on Windows!
+// #[cfg(not(windows))]
+// generate_globals_test!("go-test", "golang.org/x/tools/cmd/stringer@latest", "GOBIN");
