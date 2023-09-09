4bd8edfb2d785d2761cb66e798644b55b7240bbb
Miles Johnson <milesj@users.noreply.github.com>
2023-09-08 22:19:16 -0700

    new: Support proto v0.17. (#1)

---
+++ tests/metadata_test.rs
@@ -0,0 +1,17 @@
+use proto_pdk_test_utils::*;
+use starbase_sandbox::create_empty_sandbox;
+
+#[test]
+fn registers_metadata() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_plugin("python-test", sandbox.path());
+
+    assert_eq!(
+        plugin.register_tool(ToolMetadataInput::default()),
+        ToolMetadataOutput {
+            name: "Python".into(),
+            plugin_version: Some(env!("CARGO_PKG_VERSION").into()),
+            ..ToolMetadataOutput::default()
+        }
+    );
+}
---
+++ tests/versions_test.rs
@@ -0,0 +1,31 @@
+use proto_pdk_test_utils::*;
+use starbase_sandbox::create_empty_sandbox;
+
+generate_resolve_versions_tests!("python-test", {
+    "2.3" => "2.3.7",
+    "3.10.1" => "3.10.1",
+    "3.10" => "3.10.13",
+    "3" => "3.11.5",
+});
+
+#[test]
+fn loads_versions_from_git() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_plugin("python-test", sandbox.path());
+
+    let output = plugin.load_versions(LoadVersionsInput::default());
+
+    assert!(!output.versions.is_empty());
+}
+
+#[test]
+fn sets_latest_alias() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_plugin("python-test", sandbox.path());
+
+    let output = plugin.load_versions(LoadVersionsInput::default());
+
+    assert!(output.latest.is_some());
+    assert!(output.aliases.contains_key("latest"));
+    assert_eq!(output.aliases.get("latest"), output.latest.as_ref());
+}
